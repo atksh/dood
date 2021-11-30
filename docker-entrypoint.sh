f7ddcfd7a27803626e279cfe6115f377501f388f
atksh <31435204+atksh@users.noreply.github.com>
2021-11-30 15:04:53 +0900

    fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,6 @@
 service docker start
 git clone git@github.com:$1/$2.git
 cd $2 && git checkout $3
-ps1='\e[32m'$1/$2@$3'\e[35m:\e[34m\w \e[0m$'
+ps1='\e[32m'$1/$2@$3'\e[35m:\e[34m\w \e[0m$ '
 echo "export PS1='$ps1'" >> /root/.bashrc
 /bin/bash
