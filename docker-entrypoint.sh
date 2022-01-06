e54b991ac7bff2658347ff551304c1551ea20891
atksh <31435204+atksh@users.noreply.github.com>
2022-01-06 14:50:45 +0900

    fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,6 @@ git clone git@github.com:$1/$2.git
 cd $2 && git checkout $3
 ps1='\e[32m'$1/$2@$3'\e[35m:\e[34m\w \e[0m$ '
 echo "export PS1='$ps1'" >> /root/.bashrc
+eval $(ssh-agent)
+ssh-add ~/.ssh/id_rsa
 /bin/bash
